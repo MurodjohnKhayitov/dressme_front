7d3e99fd594f1dd2cf001a47565fa482dcec713b
IsmoilKarimov <karimoovismooil@gmail.com>
2023-03-24 18:02:38 +0500

    collection card last changes

--- src/components/home/CollectionCards.js
+++ src/components/home/CollectionCards.js
@@ -496,16 +496,12 @@ export default function CollectionCards() {
       {
         clothesSetWear ?
           <>
-            <div className="flex justify-between flex-wrap  md:mx-0 md:mt-[50px] gap-y-5 lg:gap-y-5 ">
+            <div className="flex justify-between flex-wrap  md:mx-0 md:mt-[50px] gap-y-3 lg:gap-y-5 ">
               {productList.map((data) => {
                 return (
-                  <div
-                    key={data.id}
-                    onClick={() => handleGetCardId(data?.id)}
-                    className={` ss:w-[48%] md:w-[24%] lg:w-[240px] transition ease-in-out delay-50 hover:shadow-lg ${shadowStyle} summer xs:h-[456px] lg:h-[440px] border border-solid borderColorCard overflow-hidden rounded-lg`}
-
-                  >
-                    <div className="relative w-full cursor-pointer ss:h-[206px] ls:h-[238px] xs:h-[309px] lg:h-[320px] flex content-between items-center overflow-hidden border-b border-solid flex-nowrap">
+                  <div key={data.id} onClick={() => handleGetCardId(data?.id)} 
+                  className={`w-[48.5%] border border-solid overflow-hidden rounded-lg md:w-[24%] lg:w-[240px] transition ease-in-out delay-50 hover:shadow-cardShadow ${shadowStyle} xs:h-[456px] lg:h-[440px]`}>
+                    <div className="relative w-full cursor-pointer ss:h-[206px] ls:h-[220px] ll:h-[232px] xs:h-[309px] lg:h-[320px] flex content-between items-center overflow-hidden border-b border-solid flex-nowrap">
                       {data.ProducImg ? (
                         <img className="w-full h-full m-auto hover:scale-105 transition duration-700 ease-in-out" src={data.ProducImg} alt="ProducImg" />
                       ) : (
@@ -513,84 +509,79 @@ export default function CollectionCards() {
                       )}
                       <div className="w-full flex absolute top-px p-[5px] ss:justify-end xs:justify-between">
                         <ul className="nav-lists flex-col gap-y-1 justify-center h-full ss:hidden xs:flex">
-                          <p className="group w-8 hover:w-[70px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
-                            <span className="w-8 h-8 flex items-center justify-center  ">
-                              <img src={discount} alt="" />
-                            </span>
-                            <span className=" w-0 flex items-center -mr-[1px] group-hover:w-10 duration-300  text-red-700 not-italic  font-AeonikProRegular text-[11px]">
-                              -30%
-                            </span>
-                          </p>
-                          <p className="group w-8 hover:w-[70px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
-                            <span className="w-8 h-8 flex items-center justify-center">
-                              <img src={video} alt="" />
-                            </span>
-                            <span className=" w-0 flex items-center -mr-[1px] group-hover:w-10 duration-300 not-italic  font-AeonikProRegular text-[11px]">
-                              Video
-                            </span>
-                          </p>
-                          <p className="group w-8 hover:w-[85px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
-                            <span className="w-8 h-8 flex items-center justify-center  ">
-                              <img src={delivery} alt="" />
-                            </span>
-                            <span className=" w-0 flex items-center  group-hover:w-[40px] duration-300 not-italic  font-AeonikProRegular text-[11px]">
-                              Delivery
-                            </span>
-                          </p>
+                            <p className="group w-8 hover:w-[70px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
+                                <span className="w-8 h-8 flex items-center justify-center  ">
+                                <img src={discount} alt="" />
+                                </span>
+                                <span className=" w-0 flex items-center -mr-[1px] group-hover:w-10 duration-300  text-red-700 not-italic  font-AeonikProMedium text-[11px] mt-1">
+                                -30%
+                                </span>
+                            </p>
+                            <p className="group w-8 hover:w-[70px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
+                                <span className="w-8 h-8 flex items-center justify-center">
+                                <img src={video} alt="" />
+                                </span>
+                                <span className=" w-0 flex items-center -mr-[1px] group-hover:w-10 duration-300 not-italic  font-AeonikProRegular text-[11px] mt-1">
+                                Video
+                                </span>
+                            </p>
+                            <p className="group w-8 hover:w-[85px] bg-bgCard hover:bg-white  duration-300 rounded overflow-hidden border border-borderColorCard flex items-center">
+                                <span className="w-8 h-8 flex items-center justify-center  ">
+                                <img src={delivery} alt="" />
+                                </span>
+                                <span className=" w-0 flex items-center  group-hover:w-[40px] duration-300 not-italic  font-AeonikProRegular text-[11px] mt-1">
+                                Delivery
+                                </span>
+                            </p>
                         </ul>
 
                         <div className="flex flex-col gap-y-1">
-                          <p className="w-8 h-8  rounded flex items-center bg-bgCard justify-center border border-solid border-borderColorCard hover:bg-white transition ease-out duration-500">
-                            <img src={heart} alt="" />
-                          </p>
-                          <p className="w-8 h-8 ss:hidden rounded bg-bgCard xs:flex items-center justify-center border border-solid border-borderColorCard hover:bg-white transition ease-out duration-500">
-                            <img src={bucket} alt="" />
-                          </p>
+                            <p className="w-8 h-8  rounded flex items-center bg-bgCard justify-center border border-solid border-borderColorCard hover:bg-white transition ease-out duration-500">
+                                <img src={heart} alt="" />
+                            </p>
+                            <p className="w-8 h-8 ss:hidden rounded bg-bgCard xs:flex items-center justify-center border border-solid border-borderColorCard hover:bg-white transition ease-out duration-500">
+                                <img src={bucket} alt="" />
+                            </p>
                         </div>
                       </div>
 
                       <div className="absolute w-full flex justify-between items-center px-1 bottom-0 border-solid xs:h-[38px] lg:h-8 ss:h-[30px] xs:px-2 md:px-4 bg-white hover:backdrop-brightness-125 hover:bg-white/60 transition ease-out duration-500">
                         {data?.changeColor.map((itemValue) => {
                           return (
-                            <div
-                              onClick={() => setHandleChecked(itemValue?.id)}
-                              className={`rounded-full flex items-center justify-center  ls:w-[22px] ls:h-[22px] w-5 h-5 lg:w-6 lg:h-6 ${itemValue?.colors} cursor-pointer  border border-solid	border-borderColorCard mr-[3px]`}
-                              htmlFor="Color1"
+                            <div onClick={() => setHandleChecked(itemValue?.id)} className={`rounded-full flex items-center justify-center  ls:w-[22px] ls:h-[22px] w-5 h-5 lg:w-6 lg:h-6 ${itemValue?.colors} cursor-pointer  border border-solid	border-borderColorCard mr-[3px]`} htmlFor="Color1"
                             >
                               {itemValue?.action ? (
                                 <BsCheck2Square size={15} className="text-white" />
                               ) : null}
                             </div>
-
                           );
                         })}
                       </div>
                     </div>
-                    <div className="w-full rounded-b-1xl bg-white  flex flex-wrap content-between xs:py-3 lg:p-3 ss:h-[124px] xs:h-[147px] lg:h-[120px] ss:py-2 xs:px-2 ss:px-1">
+                    <div className="w-full rounded-b-1xl bg-white  flex flex-wrap content-between px-1 xs:py-3 lg:p-3 ss:h-[124px] xs:h-[147px] lg:h-[120px] ss:py-2 xs:px-2">
                       <div className="w-full">
-                        <div className="w-full  not-italic font-AeonicProLight text-[10px] ls:text-xs lg:text-[14px] leading-4 text-black mb-3 md:mb-0  cursor-pointer">
+                        <div className="w-full  not-italic font-AeonikProRegular text-[10px] ls:text-xs lg:text-[14px] leading-4 text-black mb-3 md:mb-0  cursor-pointer">
                           {data?.title || "NoData"}
                         </div>
                         <div className="w-full flex justify-between items-center xs:mt-3">
                           <div className="flex items-center justify-between">
-                            <span>
+                            <span className="mb-[3px]">
                               {" "}
                               <img src={star} alt="" />
                             </span>
                             {/* <span><img src={} alt=""/></span> */}
                             <span className="not-italic font-AeonikProRegular text-[10px] ls:text-xs leading-4 text-right text-gray-500 ml-[2px] md:ml-1 flex items-center">
-                              <span className="font-AeonikProMedium text-[10px] ls:text-xs not-italic mx-1 text-black md:mr-[6px] md:text-[13px]">
-
+                              <span className="font-AeonikProMedium text-[10px] ls:text-xs not-italic mx-1 text-black md:text-[13px]">
                                 5.0{" "}
                               </span>
                               ({data?.starCount || 0}{" "}
-                              <span className="ss:hidden lg:block md:ml-1 md:text-[11px]">
+                              <span className="ss:hidden lg:block md:text-[11px] font-AeonikProRegular ml-1">
                                 голосов
                               </span>
                               )
                             </span>
                           </div>
-                          <div className="not-italic xs:font-AeonikProMedium ss:font-AeonikProRegular leading-4 text-black  ss:text-[11px] sm:text-xs  md:text-[13px] ">
+                          <div className="not-italic font-AeonikProRegular  leading-4 text-black ss:text-[11px] ls:text-[14px]">
                             <b>
                               <span>{data?.shirtSize || 0}</span>
                             </b>
@@ -601,11 +592,10 @@ export default function CollectionCards() {
                         <div>
                           {data.sale ? (
                             <div className="flex ss:flex-col-reverse md:flex-row	text-start items-start ">
-                              <div className="text-start m-0 p-0  not-italic font-AeonikProMedium text-[18px] md:text-base leading-1 text-red-700 xs:text-base xs:leading-4 mr-1">
+                              <div className="text-start m-0 p-0  not-italic font-AeonikProMedium text-sm ls:text-base leading-1 text-red-700 xs:text-base xs:leading-4 mr-1">
                                 {data?.sale}
                               </div>
-                              <div className="text-start m-0 p-0 text-[12px] mt-[8px]  line-through not-italic font-AeonikProRegular leading-3  text-borderColorCard ss:leading-1 md:text-[11px]">
-
+                              <div className="text-start m-0 p-0 text-[11px] ls:text-[12px] mt-[4px]  line-through not-italic font-AeonikProRegular leading-3  text-borderColorCard ss:leading-1 md:text-[11px]">
                                 {data?.price}
                               </div>
                             </div>
