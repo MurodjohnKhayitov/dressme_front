a9e7c80cbff4b015c62df021e82d63c7cba1b736
IsmoilKarimov <karimoovismooil@gmail.com>
2023-04-11 12:15:47 +0500

    some changes in set page

--- src/assets/imgs/index.js
+++ src/assets/imgs/index.js
@@ -56,11 +56,15 @@ export { default as catalogBlack } from './catalog-black.svg'
 export { default as feedbackIcon } from './feedback-icon.svg'
 export { default as inputCheckBlack } from './checked-black.svg'
 export { default as arrowRightCircle } from './arrow-right-circle.svg'
+export { default as modelToSet } from './model-to-set.svg'
 
 
 // -----Contact--------
 export { default as dashborad } from './Contact/dashboard.svg'
 export { default as locate } from './Contact/location.svg'
+
+// ------- Models --------
+export {default as model1} from './Models/model1.svg'
 
 export { default as error } from './Contact/error.svg'
 export { default as adduser } from './Contact/Add User - 1.svg'
